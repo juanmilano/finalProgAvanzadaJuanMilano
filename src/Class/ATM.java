94ac765083af231cdd3c239648feb5b72b196b2c
juanmilano <getoo.hh@gmail.com>
2018-08-05 23:56:03 -0300

    Solucionado error Saldo

--- src/Class/ATM.java
+++ src/Class/ATM.java
@@ -14,6 +14,7 @@ public class ATM {
         int retirar=0;
         int aNumber; 
         int codigo;
+   
  
         
 	        Cliente cliente;
@@ -65,7 +66,8 @@ public class ATM {
                     psw=in.nextInt();
                  if(psw == codigo ){
                  System.out.print("Ingresa la Cantidad que desea Depositar: ");
-                 saldo=in.nextInt();
+                 saldo=saldo+in.nextInt();
+                 
                  System.out.println("\t Deposito Exitoso!");
               }                
                      else
